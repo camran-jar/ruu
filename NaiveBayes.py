5c851d3217d89d36cad70a619c04c3325b215440
Cam <cameronevans1993@gmail.com>
2024-05-07 22:23:29 +1200

    Naive Begun

---
+++ NaiveBayes.py
@@ -0,0 +1,4 @@
+# This part is to implement the Naive Bayes algorithm, and evaluate the program on the breast cancer
+# dataset to be described below. The program should build a Naive Bayes classifier from the training
+# dataset and apply it to the test set.
+
